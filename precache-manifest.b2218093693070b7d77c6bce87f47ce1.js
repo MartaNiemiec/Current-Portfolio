053c2beb93c1c3d5b050a37057e76df3fc080df4
Marta Niemiec <niemiec.marta@hotmail.com>
2019-06-26 14:49:22 +0200

    Updates

---
+++ precache-manifest.b2218093693070b7d77c6bce87f47ce1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b45649ee9026c767d542af6ca624ae",
+    "url": "/Current-Portfolio/index.html"
+  },
+  {
+    "revision": "11057d83079033fe3b1e",
+    "url": "/Current-Portfolio/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "6a73e24663e562c6798c",
+    "url": "/Current-Portfolio/static/css/main.142e7ec3.chunk.css"
+  },
+  {
+    "revision": "11057d83079033fe3b1e",
+    "url": "/Current-Portfolio/static/js/2.7becb737.chunk.js"
+  },
+  {
+    "revision": "6a73e24663e562c6798c",
+    "url": "/Current-Portfolio/static/js/main.b6c40b56.chunk.js"
+  },
+  {
+    "revision": "45efe7ba0f6a00da8ecf",
+    "url": "/Current-Portfolio/static/js/runtime~main.7fc1f21a.js"
+  },
+  {
+    "revision": "835ff05682cc641efa131cda98df59cc",
+    "url": "/Current-Portfolio/static/media/bestandalucia.835ff056.jpg"
+  },
+  {
+    "revision": "d98b5bb5b019d7b494c11dc3db9a2cc5",
+    "url": "/Current-Portfolio/static/media/budgety.d98b5bb5.jpg"
+  },
+  {
+    "revision": "78dbe8cdbeeabcabc2b450a2b279d0c4",
+    "url": "/Current-Portfolio/static/media/facerecognition.78dbe8cd.jpg"
+  },
+  {
+    "revision": "da5f20f06a6e423e92800fd8bc2ef4c2",
+    "url": "/Current-Portfolio/static/media/keybord1.da5f20f0.jpg"
+  },
+  {
+    "revision": "ae7ef8734c6f066e49d51f90cdf37908",
+    "url": "/Current-Portfolio/static/media/learningplanner.ae7ef873.jpg"
+  },
+  {
+    "revision": "4849dc17a3fa86acbf04045b3806bb87",
+    "url": "/Current-Portfolio/static/media/mybooks.4849dc17.jpg"
+  },
+  {
+    "revision": "a08688b5b044d36b2e0f79c643627804",
+    "url": "/Current-Portfolio/static/media/natours.a08688b5.jpg"
+  },
+  {
+    "revision": "862ce19d784335ea400d446b7ee9380d",
+    "url": "/Current-Portfolio/static/media/piggame.862ce19d.jpg"
+  },
+  {
+    "revision": "111c7fa364c903741702d29fdc1cd012",
+    "url": "/Current-Portfolio/static/media/polind.111c7fa3.jpg"
+  },
+  {
+    "revision": "33e9117b90bfe3eedbeed36d7cac16cf",
+    "url": "/Current-Portfolio/static/media/ponggame.33e9117b.jpg"
+  },
+  {
+    "revision": "20a1082d1cbf0f678a1b184d6affe50d",
+    "url": "/Current-Portfolio/static/media/robofriends.20a1082d.jpg"
+  },
+  {
+    "revision": "ff5266e7b0e463dfb2ae8f1553ceca21",
+    "url": "/Current-Portfolio/static/media/starwars.ff5266e7.jpg"
+  },
+  {
+    "revision": "f0c9f9c3bb5df65592497baf742f10db",
+    "url": "/Current-Portfolio/static/media/trillo.f0c9f9c3.jpg"
+  }
+]);
